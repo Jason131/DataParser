1e7d6889545e87e07c9a97864c9424de8090de1c
Jason <jason131>
2019-03-02 17:34:41 -0800

    add parsing method

--- src/Utils.java
+++ src/Utils.java
@@ -1,5 +1,6 @@
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Utils {
@@ -17,5 +18,32 @@ public class Utils {
         }
 
         return output.toString();
+    }
+
+    public static ArrayList<ElectionResult> parse2016ElectionResults(String data) {
+        ArrayList<ElectionResult> result = new ArrayList<>();
+        String[] lines = data.split("\n");
+
+        for (int i = 1; i < lines.length; i++) {
+            String[] temp;
+            String line;
+
+            if (lines[i].contains("\"")) {
+                temp = lines[i].split("\"");
+                temp[1] = temp[1].replace(",", "");
+                line = temp[0] + temp[1] + temp[2];
+            } else {
+                line = lines[i];
+            }
+
+            temp = line.split(",");
+
+            ElectionResult test = new ElectionResult(Double.parseDouble(temp[1]), Double.parseDouble(temp[2]), Double.parseDouble(temp[3]), Double.parseDouble(temp[4]), Double.parseDouble(temp[5]), temp[6], temp[7], temp[8], temp[9], Integer.parseInt(temp[10]));
+            result.add(test);
+            System.out.println();
+        }
+
+        return result;
+
     }
 }
