ec1d6a10df3a9d22d0f4d313d6e87c1025d64e66
Jason <jason131>
2019-03-09 22:09:21 -0800

    add toString method

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,26 @@
 import jdk.jshell.execution.Util;
 
 import java.util.ArrayList;
+import java.util.List;
 
 /***
  * Main class for data parsers
  * @author Jason
  */
+
 public class Main {
+    private List<State> states;
+
     public static void main(String[] args) {
         String data = Utils.readFileAsString("data/2016_Presidential_Results.csv");
+        Utils.parseElection2016(data);
 
-        ArrayList<ElectionResult> result = Utils.parse2016ElectionResults(data);
+        data = Utils.readFileAsString("data/Education.csv");
+        Utils.parseEducation2016(data);
 
-        for (ElectionResult r : result) {
-            System.out.println(r.toString());
-        }
+        data = Utils.readFileAsString("data/Unemployment.csv");
+        Utils.parseEmployment2016(data);
+
+
     }
 }
